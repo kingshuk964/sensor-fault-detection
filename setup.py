535e48dd2bcf6d95912638d6c65d54566471f2c5
Kingshuk Ghosh <kingshuk.ghosh@outlook.in>
2022-12-05 10:09:23 +0000

    adding after model pusher

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+
+    """
+    this function will return list of requirements
+    """
+
+    requirement_list:List[str] = []
+    
+
+    """
+    write code to read requiremnet.txt and return list
+    """
+    # with open ("sensor-data-detection/requirements.txt",'r') as f:
+    #     for line in f:
+    #         requirement_list.append(line)
+
+    return requirement_list
+
+setup(
+
+    name="sensor",
+    version="0.0.1",
+    author="pranay",
+    author_email="pranaysawant17394@gmail.com",
+    packages= find_packages(),
+    install_requires=get_requirements() #["pymongo==4.2.0"]
+    
+)
