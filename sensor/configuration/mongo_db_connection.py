75cc4738f68a373d74770fce03b6ea2376c98ae1
kingshuk964 <78291942+kingshuk964@users.noreply.github.com>
2022-12-06 17:31:24 +0000

    Update mongo_db_connection.py

--- sensor/configuration/mongo_db_connection.py
+++ sensor/configuration/mongo_db_connection.py
@@ -1,6 +1,6 @@
 import pymongo
 from sensor.constant.database import DATABASE_NAME
-# sensor.constant.env_variable import MONGODB_URL_KEY
+from sensor.constant.env_variable import MONGODB_URL_KEY
 import certifi
 import os
 ca = certifi.where()
@@ -10,13 +10,15 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
 
-            if MongoDBClient.client is None: 
-                mongo_db_url = "mongodb+srv://Kingshuk:kingshuk1234@cluster0.yyoj4.mongodb.net/?retryWrites=true&w=majority"               
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+            if MongoDBClient.client is None:
+                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                print(mongo_db_url)
+                if "localhost" in mongo_db_url:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url) 
+                else:
+                    MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
         except Exception as e:
             raise e
-
-
